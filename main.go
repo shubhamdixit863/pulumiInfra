923949ef34deec8aaacfe3e8d37fca75fd2dbccb
shubham <shubhamdixit863@gmail.com>
2023-11-21 21:25:32 +0530

    pulumi s3 policy

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/iam"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/rds"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
 	"github.com/pulumi/pulumi-eks/sdk/go/eks"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"log"
 )
+
+const BUCKET_NAME = "gorestapi"
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
@@ -27,10 +31,56 @@ func main() {
 		// Export the cluster's kubeconfig.
 		ctx.Export("kubeconfig", cluster.Kubeconfig)
 
-		bucket, err := s3.NewBucket(ctx, "myBucket", nil)
+		bucket, err := s3.NewBucket(ctx, BUCKET_NAME, nil)
 		if err != nil {
 			return err
 		}
+		_, err = s3.NewBucketPublicAccessBlock(ctx, "bucketPublicAccessBlock", &s3.BucketPublicAccessBlockArgs{
+			Bucket: bucket.ID(),
+
+			BlockPublicAcls:       pulumi.Bool(true),
+			BlockPublicPolicy:     pulumi.Bool(false),
+			IgnorePublicAcls:      pulumi.Bool(true),
+			RestrictPublicBuckets: pulumi.Bool(false),
+		})
+		if err != nil {
+			return err
+		}
+
+		bucketPolicy := iam.GetPolicyDocumentOutput(ctx, iam.GetPolicyDocumentOutputArgs{
+			Statements: iam.GetPolicyDocumentStatementArray{
+				&iam.GetPolicyDocumentStatementArgs{
+					Actions: pulumi.StringArray{
+						pulumi.String("s3:GetObject"),
+					}, Conditions: nil,
+					Effect: pulumi.String("Allow"),
+					Principals: iam.GetPolicyDocumentStatementPrincipalArray{
+						&iam.GetPolicyDocumentStatementPrincipalArgs{
+							Type: pulumi.String("AWS"),
+							Identifiers: pulumi.StringArray{
+								pulumi.String("*"),
+							},
+						},
+					},
+					Resources: pulumi.StringArray{
+						pulumi.Sprintf("%s/*", bucket.Arn),
+					},
+					Sid: pulumi.String("PublicRead"),
+				},
+			},
+		})
+
+		// Attach the policy to the bucket
+		_, err = s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{
+			Bucket: bucket.ID(),
+			Policy: bucketPolicy.ApplyT(func(allowAccessFromAnotherAccountPolicyDocument iam.GetPolicyDocumentResult) (*string, error) {
+				return &allowAccessFromAnotherAccountPolicyDocument.Json, nil
+			}).(pulumi.StringPtrOutput),
+		})
+		if err != nil {
+			return err
+		}
+
 		ctx.Export("bucketName", bucket.ID())
 
 		// Create a new security group for RDS
@@ -68,6 +118,7 @@ func main() {
 			return err
 		}
 		ctx.Export("rdsEndpoint", rdsInstance.Endpoint)
+		log.Println("RDS Endpoint", rdsInstance.Endpoint)
 
 		return nil
 	})
